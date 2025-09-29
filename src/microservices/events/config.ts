d30e7f221559a17a2af8bedc09d4fb8ca460b724
Alex Tsurov <tsurovas@onlanta.ru>
2025-09-29 21:34:27 +0300

    fix: topics kafka

--- src/microservices/events/config.ts
+++ src/microservices/events/config.ts
@@ -4,7 +4,7 @@ export const getConfig = () => ({
   port: env.get('PORT').default(3000).asPortNumber(),
   kafka: {
     brokers: env.get('KAFKA_BROKERS').required().asArray(','),
-    topics: env.get('KAFKA_TOPICS').default('user-event,movie-event,payment-event').asArray(','),
+    topics: env.get('KAFKA_TOPICS').default('user-events,movie-events,payment-events').asArray(','),
     groupId: env.get('KAFKA_GROUP_ID').default('event-group-id').asString()
   }
 })
